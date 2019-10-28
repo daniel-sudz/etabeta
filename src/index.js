fd1f4f23f1876b4737dfbfb8cc620e93fed62751
Daniel Sudzilouski <daniel@sudzilouski.com>
2019-10-28 15:23:43 -0700

    full webpack-config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'; // eslint-disable-line no-unused-vars
+//import cal_core from '@fullcalendar/core/main.css';
+//import cal_daygrid from '@fullcalendar/daygrid/main.css';
+//import cal_timegrid from '@fullcalendar/timegrid/main.css';
+//import call_list from '@fullcalendar/list/main.css';
+require('./styles/core/main.min.css')
+require('./styles/daygrid/main.min.css')
+require('./styles/list/main.min.css')
+require('./styles/timegrid/main.min.css')
+
+var calendarEl = document.getElementById('calendar');
+if (calendarEl){
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
 
@@ -16,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
     },
-    defaultDate: '2018-01-12',
+    //defaultDate: '2018-01-12',
     navLinks: true, // can click day/week names to navigate views
     editable: true,
     eventLimit: true, // allow "more" link when too many events
@@ -25,5 +37,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
   });
 
-  calendar.render();
+   calendar.render();
 });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,59 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob')
 const path = require('path')
-const CompressionPlugin = require('compression-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  resolve: {
-    extensions: [ '.js' ]
-  },
+module.exports = { 
+  //target: 'node', // in order to ignore built-in modules like path, fs, etc. 
+  //externals: [nodeExternals()], // in order to ignore all modules in node_modules folder 
+  mode: "production",
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
+    filename: "[name].js",
+    path: __dirname + "/dist"
   },
-  devtool: 'sourcemap',
+  module: {
+    rules: [
+      //load svg resource files
+      {
+        test: /\.svg$/,
+        loader: 'file-loader'
+      },
+      // javascript = babel + uglify
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/, ///(node_modules|bower_components)/, //'/node_modules/core-js/',
+        use: [{ loader: "babel-loader" }]
+      },
+
+      // css file: extract to css file with mini extract plugin
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+}, 
+
+
+  // plugin
   plugins: [
-  new CompressionPlugin()
-  ],
-}
+    new CompressionPlugin(),
+    //new HtmlWebpackPlugin(), // Generates default index.html
+	new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      filename: 'calendar.html',
+      template: './src/calendar.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(`${path.join(__dirname, 'src')}/**/*`,  { nodir: true }),
+    }),
+	//new BundleAnalyzerPlugin(),
+  ]
+};
