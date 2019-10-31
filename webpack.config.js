c9b2147b75e0a90c7ab4b8e9690e4914117a93c7
Daniel Sudzilouski <daniel@sudzilouski.com>
2019-10-31 09:30:28 -0700

    calendar scaling does not work as expected

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const CompressionPlugin = require('compression-webpack-plugin')
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const PurgecssPlugin = require('purgecss-webpack-plugin')
 const glob = require('glob')
 const path = require('path')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = { 
   //target: 'node', // in order to ignore built-in modules like path, fs, etc. 
   //externals: [nodeExternals()], // in order to ignore all modules in node_modules folder 
+
   mode: "production",
   entry: "./src/index.js",
   output: {
@@ -24,11 +27,7 @@ module.exports = {
         loader: 'file-loader'
       },
       // javascript = babel + uglify
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/, ///(node_modules|bower_components)/, //'/node_modules/core-js/',
-        use: [{ loader: "babel-loader" }]
-      },
+
 
       // css file: extract to css file with mini extract plugin
       {
@@ -55,19 +54,26 @@ module.exports = {
       filename: "[name].css",
       chunkFilename: "[id].css"
     }),
-    new PurgecssPlugin({
-      paths: glob.sync(`${path.join(__dirname, 'src')}/**/*`,  { nodir: true }),
-    }),
+    //new PurgecssPlugin({ 
+    //  paths: glob.sync(`${path.join(__dirname, 'src')}/**/*`,  { nodir: true }), //Purgecss is great but it treeshakes some necessary items like bootbox alert boxes
+    //}),
 	
 	new UglifyJsPlugin({
-        test: /\.js$/,
-        sourceMap: false,
-        uglifyOptions: {
-			dead_code: true,
-            compress: {},
-            mangle: true,
-        }
-	}),
+                sourceMap: true,
+                uglifyOptions: {
+                    dead_code: true
+                }
+            }),
+	
+	new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default'],
+        // In case you imported plugins individually, you must also require them here:
+        Util: "exports-loader?Util!bootstrap/js/dist/util",
+        Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      }),
 	
 	//new BundleAnalyzerPlugin(),
   ]
