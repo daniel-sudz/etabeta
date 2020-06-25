7a3609b82d3bd2d7e6ee626d04a7f820655fc793
daniel-sudz <52898838+daniel-sudz@users.noreply.github.com>
2020-06-25 21:14:52 +0000

    Deploying to gh-pages from  @ e540dd4b5509410d624eb8c935514f225824a297 🚀

---
+++ precache-manifest.49d69f6d785b849931d61f9582aecbaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c6f94812d5fa3bb707b048ccd568f15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ef145a156e2c8abfe02",
+    "url": "/static/css/2.fa7a0ad5.chunk.css"
+  },
+  {
+    "revision": "fe13bcf5f4fcedb076d0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2ef145a156e2c8abfe02",
+    "url": "/static/js/2.ff8f82dc.chunk.js"
+  },
+  {
+    "revision": "0fc8bb94920020b9841724b77c5e3584",
+    "url": "/static/js/2.ff8f82dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe13bcf5f4fcedb076d0",
+    "url": "/static/js/main.cfb89465.chunk.js"
+  },
+  {
+    "revision": "f19a091faf781af5e2aa",
+    "url": "/static/js/runtime-main.2375fa69.js"
+  }
+]);
